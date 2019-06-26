7a4c6de9165bea6be734a0a67cd3747c566b7c02
Tianhan Lu <tianhan.lu@colorado.edu>
2019-06-25 21:03:19 -0600

    add 007

---
+++ 007/Solution02.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int reverse(int x) {
+        int MIN = 0x80000000; // -2^31
+        int MAX = 0x7FFFFFFF;
+        if (x==MIN) return 0; // To make sure that -x is a non-negative number
+        int num = x>=0 ? x : -x;
+        // assert(num>=0);
+        int digits = 1;
+        for (int tmp=num; tmp>9; tmp/=10) {
+            digits++;
+        }
+        int res = 0; // res>=0
+        for (int i=0; i<digits; i++) {
+            int tens = num%10;
+            for (int j=1; j<=digits-1-i; j++) {
+                if (tens>(MAX/10)) return 0;
+                else tens *= 10;
+            }
+            // assert(tens>=0);
+            int tmp = res+tens;
+            boolean overflow = false;
+            if (tmp < 0) {
+                if (x<0 && tmp==MIN) ;
+                else overflow = true;
+            }
+            if (overflow) return 0;
+            else res = tmp;
+            // assert(res>=0);
+            num = num/10;
+        }
+        if (x<0) {
+            if (res==MIN) ;
+            else res = -res;
+        }
+        return res;
+    }
+}
+
+// Runtime: 1 ms, faster than 100.00% of Java online submissions for Reverse Integer.
+// Memory Usage: 33.6 MB, less than 25.92% of Java online submissions for Reverse Integer.
