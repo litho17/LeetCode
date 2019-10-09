2f2f4acc7d774f86ca93b3b9dbf13e4924b23576
Tianhan Lu <tianhan.lu@colorado.edu>
2019-10-09 08:15:04 -0700

    update 042

--- 042/Solution.java
+++ 042/Solution.java
@@ -6,6 +6,8 @@ class Solution {
         int left = 0;
         int right = left+1;
         while (left<len) {
+			// Find the farthest right index s.t. for all heights indexed by [left+1,right-1],
+            // we have height[i]<=height[left] && height[i]<=height[right]
             boolean foundPit = false;
             right = len-1;
             while (right>=left+1) {
@@ -40,3 +42,7 @@ class Solution {
 
 // Runtime: 3 ms, faster than 18.97% of Java online submissions for Trapping Rain Water.
 // Memory Usage: 37.3 MB, less than 98.63% of Java online submissions for Trapping Rain Water.
+
+
+// Another solution would be that, each height (with index i) can collect water iff. 
+// there exist index l and r s.t. 0<=l<i && len>r>i && height[l]>=height[i] && height[r]>=height[i]
