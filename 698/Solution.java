bca77527e028144b6f115892d6584c1df7d7156f
Tianhan Lu <tianhan.lu@colorado.edu>
2019-10-19 11:18:36 -0700

    add 698

---
+++ 698/Solution.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean canPartitionKSubsets(int[] nums, int k) {
+        Arrays.sort(nums);
+        int[] sums = new int[k];
+        int sum = 0;
+        for (int i=0; i<nums.length; i++) {
+            sum += nums[i];
+        }
+        if (sum%k != 0) return false;
+        return helper(nums, sums, nums.length-1, sum/k);
+    }
+    
+    boolean helper(int[] nums, int[] sums, int idx, int target) {
+        if (idx==-1) {
+            for (int i=0; i<sums.length; i++) {
+                // Check if the sum is equal to target and if the subset is non-empty
+                if (sums[i] != target || sums[i] == 0) return false;
+            }
+            return true;
+        }
+        for (int i=0; i<sums.length; i++) {
+            if (sums[i]+nums[idx] <= target) {
+                int oldSum = sums[i];
+                sums[i] = sums[i]+nums[idx];
+                boolean res = helper(nums, sums, idx-1, target);
+                sums[i] = oldSum;
+                if (res) return true;
+            }
+        }
+        return false;
+    }
+}
+
+// Runtime: 15 ms, faster than 36.43% of Java online submissions for Partition to K Equal Sum Subsets.
+// Memory Usage: 34.6 MB, less than 81.40% of Java online submissions for Partition to K Equal Sum Subsets.
